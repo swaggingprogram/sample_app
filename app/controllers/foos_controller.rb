41647370888ab283ca28c2b671d7ccf19b4bb36d
swaggingprogram <ogura0908work@gmail.com>
2021-12-08 15:57:09 +0900

    delete controller foos

--- app/controllers/foos_controller.rb
+++
@@ -1,2 +0,0 @@
-class FoosController < ApplicationController
-end
--- test/controllers/foos_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class FoosControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
