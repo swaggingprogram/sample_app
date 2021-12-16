e93ab0d5b66fe1e0d6f7fdcedd2e01413e17fc5c
swaggingprogram <ogura0908work@gmail.com>
2021-12-16 18:15:59 +0900

    細かい修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,11 @@
 class SessionsController < ApplicationController
+  include SessionsHelper
   def new
   end
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user &. user.authenticate(params[:session][:password])
+    if user&.authenticate(params[:session][:password])
       log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       remember user
